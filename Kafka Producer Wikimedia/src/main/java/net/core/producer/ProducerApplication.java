8d4e23710a677ad83a35236572a2c112426cda31
surajkumar <jnvsuraj7388@gmail.com>
2024-07-17 11:09:09 +0530

    Implemented wikimedia producer using kafka

--- Kafka Producer Wikimedia/src/main/java/net/core/producer/ProducerApplication.java
+++ Kafka Producer Wikimedia/src/main/java/net/core/producer/ProducerApplication.java
@@ -1,11 +1,19 @@
 package net.core.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class);
+    }
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
---
+++ Kafka Producer Wikimedia/src/main/java/net/core/producer/WikimediaChangesHandler.java
@@ -0,0 +1,38 @@
+package net.core.producer;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class WikimediaChangesHandler implements EventHandler {
+    private static final Logger logger = LoggerFactory.getLogger(WikimediaChangesHandler.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private String topic;
+    public WikimediaChangesHandler(KafkaTemplate<String, String> kafkaTemplate, String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+            logger.info("Event data : "+ messageEvent.getData());
+            kafkaTemplate.send(topic, messageEvent.getData());
+    }
+    @Override
+    public void onOpen() throws Exception {
+    }
+    @Override
+    public void onClosed() throws Exception {
+    }
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+}
